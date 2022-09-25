dbad9ae0f302c412707ee829987a42ac44a9f7a0
Chamithu Ruberu <109074596+ChamithuRuberu@users.noreply.github.com>
2022-09-25 10:50:28 +0530

    Initial commit

---
+++ src/com/fullstackChatbot/main.java
@@ -0,0 +1,20 @@
+package com.fullstackChatbot;
+
+import com.sun.corba.se.spi.presentation.rmi.StubAdapter;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
+import java.util.HashMap;
+
+public class main {
+    public static void main(String[] args) {
+        Spark.staticFileLocation("public_html");
+
+
+        Spark.get("/",(request, response) ->{
+            HashMap<String,Object>model=new HashMap<>();
+                    return new ThymeleafTemplateEngine().render(new ModelAndView(model,"index"));
+        });
+    }
+}
